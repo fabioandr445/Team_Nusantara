b89e7be07813a2fb442d816cfd3e15c7b7f3614a
isnainiheryasa <isnaini_heryasa@teknokrat.ac.id>
2025-11-23 21:16:27 +0700

    isnainiheryasa:menambahkan register/page.tsx route

---
+++ code/frontend/app/register/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import Link from "next/link";
+
+export default function LoginPage() {
+  const router = useRouter();
+  const [u, setU] = useState("");
+  const [p, setP] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    router.push("/dashboard");
+  };
+
+ 
+}
